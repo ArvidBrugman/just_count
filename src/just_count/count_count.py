6826cfbed8d639c90b551fd001fd87a587ee540e
ArvidBrugman <arvid.brugman@student.uva.nl>
2024-11-15 13:40:33 +0100

    click command

--- src/just_count/count_count.py
+++ src/just_count/count_count.py
@@ -1,11 +1,11 @@
-import just_count.square as square
 import click
+import square
 
 @click.command()
-@click.argument("getal")
+@click.argument("getal", type=int)
 def main(getal):
     print(f"The square of {getal} is {square.square(getal)}")
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
